3c833f458ded44a2f035da7a2cc9f115463eb2e2
Khubbatov Rustem <hubbatov@mail.ru>
2023-01-17 19:46:28 +0000

    removed all user-based info

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-read -p "Enter login: " LOGIN
-read -s -p "Enter password: " PASSWORD
-
 ENDPOINT="https://webdav.yandex.ru/"
 BACKUP_NAME=jarvis_backup_$(date +"%Y%m%d_%H%M%S")
 
@@ -14,17 +11,13 @@ echo "Waiting..."
 
 mkdir -p $BACKUP_NAME/{home-assistant/config,mqtt}
 
-sudo cp home-assistant/config/secrets.yaml $BACKUP_NAME/home-assistant/config/secrets.yaml
-sudo cp home-assistant/config/known_devices.yaml $BACKUP_NAME/home-assistant/config/known_devices.yaml
-sudo cp -r home-assistant/config/.storage/ $BACKUP_NAME/home-assistant/config/.storage/
-
-sudo cp -r mqtt/ssl/ $BACKUP_NAME/mqtt/ssl/
+sudo cp -r home-assistant/config/ $BACKUP_NAME/home-assistant/config/
+sudo cp -r mqtt/ $BACKUP_NAME/mqtt/
 
 sudo tar czf $BACKUP_NAME.tar.gz $BACKUP_NAME
 
 sudo rm -rf ./$BACKUP_NAME
 curl --progress-bar -o /dev/stdout --verbose --header "Authorization: OAuth $TOKEN" -T $BACKUP_NAME.tar.gz $ENDPOINT/$BACKUP_NAME.tar.gz
-#curl --header "Authorization: OAuth $TOKEN" -T $BACKUP_NAME.tar.gz $ENDPOINT/$BACKUP_NAME.tar.gz
 sudo rm ./$BACKUP_NAME.tar.gz
 
 echo "Done"
