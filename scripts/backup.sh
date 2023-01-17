c2ee82d9e2f48fb1079f71bb32c88e57cdb60963
Khubbatov Rustem <hubbatov@mail.ru>
2023-01-17 20:22:33 +0000

    fix error in backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -10,13 +10,13 @@ echo "Waiting..."
 
 mkdir -p $BACKUP_NAME/{home-assistant/config,mqtt}
 
-sudo cp -r home-assistant/config/ $BACKUP_NAME/home-assistant/config/
-sudo cp -r mqtt/ $BACKUP_NAME/mqtt/
+sudo cp -r home-assistant/config/* $BACKUP_NAME/home-assistant/config
+sudo cp -r mqtt/* $BACKUP_NAME/mqtt
 
 sudo tar czf $BACKUP_NAME.tar.gz $BACKUP_NAME
 
 sudo rm -rf ./$BACKUP_NAME
-curl --progress-bar -o /dev/stdout --verbose --header "Authorization: OAuth $TOKEN" -T $BACKUP_NAME.tar.gz $ENDPOINT/$BACKUP_NAME.tar.gz
+curl --progress-bar -o /dev/stdout --header "Authorization: OAuth $TOKEN" -T $BACKUP_NAME.tar.gz $ENDPOINT/$BACKUP_NAME.tar.gz
 sudo rm ./$BACKUP_NAME.tar.gz
 
 echo "Done"
