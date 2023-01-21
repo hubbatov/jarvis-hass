3a083e2758137105eba76d782bec79775056b0cc
Khubbatov Rustem <hubbatov@mail.ru>
2023-01-21 10:30:18 +0000

    adding prometheus

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,7 +11,9 @@ echo "Waiting..."
 mkdir -p $BACKUP_NAME/{home-assistant/config,mqtt}
 
 sudo cp -r home-assistant/config/* $BACKUP_NAME/home-assistant/config
-sudo cp -r mqtt/* $BACKUP_NAME/mqtt
+sudo cp -r mqtt/ssl/* $BACKUP_NAME/mqtt/ssl/*
+sudo cp scripts/.yandex_token BACKUP_NAME/scripts/.yandex_token
+sudo cp prometheus/.hass_bearer_token
 
 sudo tar czf $BACKUP_NAME.tar.gz $BACKUP_NAME
 
